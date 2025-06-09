0557214d6d38ef5c221f9662b05dd4c497be57a1
shyamboraste <borasteshyam44@gmail.com>
2025-06-09 16:38:39 +0000

    conversion data types

---
+++ 01_basics/03_ConversionOperator.js
@@ -0,0 +1,32 @@
+let score = "33abc"
+console.log(score)
+console.log(typeof score)
+console.log(typeof(score))
+
+// number conversion
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+// "33" easily convert => number
+//"33abc" => NaN (not in number)
+
+
+
+//boolean conversion
+let isLoggedIn =""
+let booleanIsLogged = Boolean(isLoggedIn)
+console.log(booleanIsLogged);
+
+// 1 => true ; 0 => false
+//"" => false
+//"shyam" => true
+
+
+//string conversion
+let Sky = 33
+let Stringnumber = String(Sky)
+console.log(Stringnumber)
+console.log(typeof(Stringnumber))
+
+// outptu : (typeof) 33 => string 
