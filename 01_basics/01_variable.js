4ffba9bd140b62c6d11d5af389cfa13c5f1ad3c0
shyamboraste <borasteshyam44@gmail.com>
2025-06-09 16:36:17 +0000

    data types

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,1 +1,19 @@
+const MyId = 12344843
+// accountId = 2   // not allowed always define the type of variable 
 
+let Password = "12345";
+var Destination = "USA";
+// prefer not use to var 
+// because of issue in block scope and functional scope
+
+HolderName = "shyam";  // posibility to declare variable 
+
+let City;   // output : undefined
+
+console.log(MyId)
+console.log(Password)
+console.log(Destination)
+console.log(HolderName)    
+
+
+console.table([MyId,Password,Destination,HolderName,City]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,49 @@
+"use strict" //treat all js code as NEWER version 
+
+// This is a wrong way to write code,code redabilty is very worst
+console.log(3+3)
+console.log(3 + 3); console.log("sham");
+
+// correct way to write code 
+console.log(3 + 3);
+console.log("shyam");
+
+console.log( 3  
+      +  3
+)
+// code redeaibility should be high
+
+
+//dataTypes 
+
+//number => 2 to power 53
+let age = 20;
+console.log(typeof age) //  output : number
+
+//string
+let Name = "shaym";
+console.log(typeof Name) // outpur : string
+
+//boolean => true || false
+let islogged = true;
+console.log(typeof islogged) // output : boolean
+
+//undefined
+let state;
+console.log(typeof undefined); // outpur : undefined
+
+//null  => standaloan value
+let temprerature = null;
+console.log(typeof null); // output : object
+
+
+//bigint
+ 
+//symbol => unique 
+
+//object
+
+
+
+//documentation  ecma script or mdn 
+//ecma script r 2023 languge specifiaction  //link http://tc39.es/ecma262/#sec-intr
