541e971f1beeb2a2b0d32b095adc0ac38180fc8c
shyam boraste <borasteshyam44@gmail.com>
2025-06-13 16:46:37 +0000

    numbers and math

---
+++ 01_basics/07_num_and_math.js
@@ -0,0 +1,33 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)       // using new keyword to create a object
+//  console.log(balance);
+//  console.log(balance.toString().length); //output = 100 3
+//  console.log(balance.toFixed(1));        //output = 100.0
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));   // output : 123.9
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN')); // output = 10,00,000
+    //below you see the value 1000000, using the tolocalestring() function to convert this value into the indian format like =>  10,00,000
+
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+//  console.log(Math);
+//  console.log(Math.abs(-4));         // output = 4  // abs=> absolute() fun is used to convert negative value to positive
+//  console.log(Math.round(4.6));         // output = 5 // round() fun returns the round figure number
+//  console.log(Math.ceil(4.2));          // output = 5 //ceil() fun return the large value to given input of round figure
+//  console.log(Math.floor(4.9));         // output = 4 // floor() fun is opposite to ceil
+//  console.log(Math.min(4, 3, 6, 8));    // outptu = 3
+//  console.log(Math.max(4, 3, 6, 8));    // outptu = 8
+//  console.log(Math.random());           // gerating the random numbers between 0 to 10 ex.0.58742
+//  console.log((Math.random()*10) + 1);  //random no *10 +1 ==>give at list more than 1 value
+//  console.log(Math.floor(Math.random()*10) + 1);
+
+// const min = 10
+// const max = 20
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+// minimum accepted value 10 
