2e9e71e6c4bf36b2f37abb7dc57c0f1d816d39cf
shyam boraste <borasteshyam44@gmail.com>
2025-06-21 16:54:43 +0000

    switch case statement

---
+++ 04_control_flow/02-switch.js
@@ -0,0 +1,49 @@
+//      switch case statement 
+
+const month = 4
+
+switch(month)
+{
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("mar")
+        break;
+    case 4:
+        console.log("april")
+        break;
+    case 5:
+        console.log("may")
+        break;
+
+    default: 
+    console.log("value not match");
+    break;
+}
+// break statement used to stop the control flow of the code 
+
+const monthh = "mar"
+
+switch(monthh)
+{
+    case "jan":
+        console.log("jan")
+        break;
+    case "feb":
+        console.log("feb")
+        break;
+    case "mar":
+        console.log("mar")
+        break;
+    case "april":
+        console.log("april")
+        break;
+    default: 
+    console.log("value not match");
+    break;
+}
+// break statement used to stop the control flow of the code 
