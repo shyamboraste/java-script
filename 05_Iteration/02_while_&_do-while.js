b973c084b38e6148a68147f97591879f66553c50
shyam boraste <borasteshyam44@gmail.com>
2025-06-22 16:35:55 +0000

    while and do while loop

---
+++ 05_Iteration/02_while_&_do-while.js
@@ -0,0 +1,57 @@
+// eikach kam apan vegveglya loop ne karu shakto at least 2 loop mastr karave lagtil
+// all loop working same but difference is only syntax 
+// at least two loops learn proper
+// while loop
+
+let index = 0;
+
+while (index <= 10) {
+    console.log(`value of index is: ${index}`);
+    index = index + 2
+}
+
+// output :
+// value of index is: 0
+// value of index is: 2
+// value of index is: 4
+// value of index is: 6
+// value of index is: 8
+// value of index is: 10
+
+
+let myarray = ['flash','batman','superman']
+
+let arr = 0;
+while (arr < myarray.length) {
+    console.log(`value of : ${myarray[arr]}`)
+    arr = arr + 1
+    
+}
+
+//output : 
+// value of : flash
+// value of : batman
+// value of : superman
+
+
+let score = 1
+do{
+console.log(`score is ${score}`)
+score++;
+}while(score <= 10);
+
+//output : 
+// score is 1
+// score is 2
+// score is 3
+// score is 4
+// score is 5
+// score is 6
+// score is 7
+// score is 8
+// score is 9
+// score is 10
+
+// while loop is at least one time is executed if he not check the value is true or false
+
+
