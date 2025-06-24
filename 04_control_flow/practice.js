9e6892069faaefecfeee872393c1e34b71ede631
shyam boraste <borasteshyam44@gmail.com>
2025-06-24 16:37:33 +0000

    control flow code file

---
+++ 04_control_flow/practice.js
@@ -0,0 +1,59 @@
+if(balance < 30000){
+    // console.log("less than 30000")
+}else if(balance < 2000){
+    // console.log("less than 2000")
+}else if(balance < 500){
+    // console.log("less than the 500")
+}else{
+    // console.log("enter valid amount")
+}
+
+
+
+const month = 4
+
+switch(month)
+{
+    case 1:
+        console.log("jan")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("mar")
+        break;
+    case 4:
+        console.log("april")
+        break;
+    case 5:
+        console.log("may")
+        break;
+
+    default: 
+    console.log("value not match");
+    break;
+}
+// break statement used to stop the control flow of the code 
+
+const monthh = "mar"
+
+switch(monthh)
+{
+    case "jan":
+        console.log("jan")
+        break;
+    case "feb":
+        console.log("feb")
+        break;
+    case "mar":
+        console.log("mar")
+        break;
+    case "april":
+        console.log("april")
+        break;
+    default: 
+    console.log("value not match");
+    break;
+}
+// break statement used to stop the control flow of the code 
