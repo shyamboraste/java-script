640ee15f66856cfde88309b7da7c0fe401ed7f52
shyam boraste <borasteshyam44@gmail.com>
2025-07-03 05:10:31 +0000

    bg color changer

---
+++ 07_JS projects for beginners level/backgroung color changer/script.js
@@ -0,0 +1,33 @@
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(function (button) {
+    console.log(button);
+    button.addEventListener('click' , function(e) {
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'pink'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'red'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'green'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
+    });
+});
+
+
+
+
+
+
+
+
