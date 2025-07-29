b106f2afa1de0fa9fdffe0f1ab39e4a0e6d42aa2
shyam boraste <borasteshyam44@gmail.com>
2025-07-29 14:48:53 +0000

    get set with function

---
+++ 09_advance_one/13_properties_get_set.js
@@ -0,0 +1,29 @@
+// get set with function
+function User(email , password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get : function() {
+            return this._email.toUpperCase()
+        },
+        set : function(values) {
+             this._email = values
+        }
+    })
+
+    Object.defineProperty(this, 'password', {
+        get : function() {
+            return this._password.toUpperCase()
+        },
+        set : function(values) {
+             this._password = values
+        }
+    })
+}
+
+const chai = new User("sjddjc@.com","23756")
+console.log(chai.email);
+console.log(chai.password);
+
+
